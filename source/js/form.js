0021216b60f81362b22404ef4f9e3f45a7333071
Stalker <pleshivtsev.denis@yandex.ru>
2018-05-27 00:18:51 +0300

    commit23

--- source/js/form.js
+++ source/js/form.js
@@ -15,5 +15,32 @@
     me.close = function() {
         form.classList.add('is-hidden');
     }
-    window.form = me;
-}())
+    me.isValid = function() {
+        var requiredFields = document.querySelectorAll('[data-valid=required]');
+        var emailValue = document.querySelector('[data-email]').value;
+        var numberValue = document.querySelector('[data-number]').value;
+        if (!me.isAllCompleted(requiredFields)) {
+            console.log('Заполните все необходимые поля');
+            return false;
+        } else if (!ITVDN.validation.isEmail(emailValue)) {
+            console.log('Не верный email');
+            return false;
+        } else if (!ITVDN.validation.isNumber(numberValue)) {
+            console.log('Не верный номер');
+            return false;
+        }
+
+        return true;
+    }
+    me.isAllCompleted = function(data) {
+        var result = true;
+        for (var i = 0; i < data.length; i++) {
+            if (!ITVDN.validation.isNotEmpty(data[i].value)) {
+                result = false;
+                break
+            }
+        }
+        return result;
+    }
+    ITVDN.form = me;
+}());
--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,26 @@
 (function() {
     var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
     if (openFormButton) {
         openFormButton.addEventListener('click', function(e) {
             e.preventDefault();
-            form.open();
+            ITVDN.form.open();
         })
     }
-}())
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            if (ITVDN.form.isValid()) {
+                console.log('Is good');
+            } else {
+                console.log('Is not valid');
+            }
+            e.preventDefault();
+        })
+    }
+    document.onkeydown = function(e) {
+        if (e.keyCode == 27) { // escape
+            ITVDN.form.close();
+        }
+    }
+
+}());
